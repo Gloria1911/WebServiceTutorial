6e176310e3d3e3c4d99fa404a1e5c6b1e659df15
Gloria Ticini <gloriaticini@outlook.it>
2019-12-19 12:46:09 +0100

    modifica

---
+++ WebServiceTutorial/WebServiceTutorial/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebServiceTutorial
+{
+    public static class Constants
+    {
+        public const string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public const string OpenWeatherMapAPIKey = "abd0637b19d7e31c2f5a03b552692778";
+    }
+}
